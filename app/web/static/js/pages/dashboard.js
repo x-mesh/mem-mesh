9d324185fc26401614b4212f807c7f604f3796da
Jinwoo <jinwoo@parametacorp.com>
2026-06-25 14:37:39 +0900

    chore: add toast notification on memory creation

--- app/web/static/js/pages/dashboard.js
+++ app/web/static/js/pages/dashboard.js
@@ -4,6 +4,7 @@
  */
 
 import { wsClient } from '../services/websocket-client.js';
+import { showToast } from '../utils/toast-notifications.js';
 
 const CAT_ICONS = {
   task: '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 11l3 3L22 4"/><path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"/></svg>',
@@ -124,6 +125,7 @@ class DashboardPage extends HTMLElement {
     this.refreshStats();
     // Push viz event
     this._pushVizEvent(memory);
+    showToast('New memory created', 'success');
   }
 
   onMemoryUpdated({ memory_id, memory }) {
